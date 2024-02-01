62aebb3dc63eddc3bbf40acb023a55d4beb9ea1e
Anchal Deshmukh <anchaldeshmukh789@gmail.com>
2024-02-01 23:47:47 +0530

    success

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,12 +4,9 @@ import axios from "axios"
 const Login = () => {
   const [emailInput, setEmailInput] = useState("");
   const [passwordInput, setPasswordInput] = useState("");
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   // const [responseData, setResponseData] = useState();
-  let avatar = "https://picsum.photos/800";
-  const loginHandler = async (event) => {
+
+  const submitHandler = async (event) => {
     event.preventDefault();
     console.log(emailInput, passwordInput);
     const url = "https://api.escuelajs.co/api/v1/auth/login";
@@ -18,20 +15,10 @@ const Login = () => {
     console.log(response);    
   };
 
-  const registerHandler = ()=>{
-    
-  }
-
   return (
     <div className="loginPage">
-      <form>
-        <input type="text" value={name} onChange={(event)=>{setName(event.target.value)}}/>
-        <input type="text" value={email} onChange={(event)=>{setEmail(event.target.value)}}/>
-        <input type="text" value={password} onChange={(event)=>{setPassword(event.target.value)}}/>
-        <button onClick={registerHandler}>Register</button>
-      </form>
       <div className="loginContainer">
-        <form>
+        <form action="" method="post">
           <input
             type="text"
             value={emailInput}
@@ -46,7 +33,7 @@ const Login = () => {
               setPasswordInput(event.target.value);
             }}
           />
-          <button type="submit" onClick={(event) => loginHandler(event)}>
+          <button type="submit" onClick={(event) => submitHandler(event)}>
             Submit
           </button>
         </form>
